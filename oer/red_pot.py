0563a9999fc5ac098981d750aa7f1545bbf142de
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2024-01-07 11:45:48 -0500

    upload

--- oer/red_pot.py
+++ oer/red_pot.py
@@ -2,15 +2,15 @@ import numpy as np
 
 
 def electrochem_potential(reactant, product):
-    return (product - reactant) * ev2kjmol * kjmol2v
+    return (product - reactant) * ev2kjmol
 
 
 # Free energy
 g1_ni4oh3 = -930.63454
-g2_ni4oh3_oh = -940.296283
-g3_ni4oh3_o = -936.858766
-g4_ni4oh2_o_o = -931.844656
-g5_ni4oh3_o2 = -940.488191
+g2_ni4oh3_oh = -940.176367
+g3_ni4oh3_o = -936.79403
+g4_ni4oh2_o_o = -931.779148
+g5_ni4oh3_o2 = -940.239779
 g91_oh = -10.047892
 g92_h2o = -14.324212
 g93_o2 = -9.93605306
